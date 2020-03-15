5ac5daf75a854bc4bc6d4a60ba25a093ea8a36c2
Arif Setyawan <me@arifsetyawan.com>
2020-03-15 18:40:28 +0800

    update application

--- packages/application/src/app.js
+++ packages/application/src/app.js
@@ -1,5 +1,5 @@
 const worldUtil = require("@arif/utilities");
 
 module.exports.appSayHello = () => {
-  return `hello ${worldUtil.worldUtil.world()}`
+  return `hello ${worldUtil.worldUtil.world()} ${worldUtil.worldUtil.jane()}`
 };
